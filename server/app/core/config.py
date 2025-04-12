49e74561c84414114868eb59683924c8f04ab689
mintuchel <max3627@naver.com>
2025-04-12 18:41:57 +0900

    add: pinecone configs

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -2,7 +2,8 @@ from pydantic_settings import BaseSettings
 
 class Configurations(BaseSettings):
     openai_api_key: str
-
+    pinecone_api_key: str
+    
     class Config:
         env_file = ".env"
 
