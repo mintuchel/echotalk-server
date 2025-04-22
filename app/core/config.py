eb8d231973f18738114cc660b90f7f4327f5130a
mintuchel <max3627@naver.com>
2025-04-22 11:16:49 +0900

    add: database url in config

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,8 @@ from pydantic_settings import BaseSettings
 class Configurations(BaseSettings):
     openai_api_key: str
     pinecone_api_key: str
-    
+    mysql_url: str
+
     class Config:
         env_file = ".env"
 
